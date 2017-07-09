5748660802c0fb668295e8452fff9f2aab83227c
jonathandodard <jonathan.dodard@gmail.com>
2017-07-09 16:02:00 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+/**
+ * Created by jonathan on 05/05/17.
+ */
+circle('subBar1', 100, '#dddddd', '#000000', false);
+circle('subBar2', 100, '#bababa', '#000000', false);
+circle('subBar3', 100, '#bababa', '#000000', false);
+circle('subBar4', 100, '#bababa', '#000000', false);
+circle('subBar5', 100, '#bababa', '#000000', false);
+circle('subBar6', 100, '#bababa', '#000000', false);
+circle('subBar7', 100, '#bababa', '#000000', false);
+
+circle('bar1', 100, '#e44d26', '#e42d1a', true);
+circle('bar2', 65, '#010101', '#4b4b4b', true);
+circle('bar3', 80, '#777bb3', '#9398d6', true);
+circle('bar4', 80, '#777bb3', '#9398d6', true);
+circle('bar5', 80, '#777bb3', '#9398d6', true);
+circle('bar6', 80, '#777bb3', '#9398d6', true);
+circle('bar7', 80, '#777bb3', '#9398d6', true);
